97370a047086b85f7521edc63c7caae2dc00b9ca
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2015-12-08 15:41:03 +0530

    Defined preliminary configs.

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='pyzure-cli',
+    version='0.0',
+    description=(
+        'A CLI tool for Microsoft Azure Storage',
+        'and Service Bus operations, built on Azure'
+    )
+    url='http://github.com/hashfyre/pyzure-cli',
+    author='Joy Bhattacherjee',
+    author_email='joy.bhattacherjee@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: System Administrators',
+        'Topic :: System :: Systems Administration',
+        'License :: OSI Approved :: MIT License'
+    ],
+    packages=['pyzure-cli'],
+    keywords='cli azure',
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'pyzure-cli=pyzure-cli:main',
+        ],
+    }
+)
