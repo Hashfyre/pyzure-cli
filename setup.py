a49d0d3fe48790c8cfc4bdadc5a0cedb169604f3
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2015-12-20 15:30:21 +0530

    Reflected rename in  and actually properly renamed inner module.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     install_requires=[],
     entry_points={
         'console_scripts': [
-            'pyzure-cli=pyzure-cli:main',
+            'pyzure=pyzure:main',
         ],
     }
 )
